0905c824b4429204a7333f8b5aa72650bc004ab1
Kevin Wonus <kevin@wonus.com>
2023-02-19 22:30:58 -0500

    Initial implementation of Blueprint-Blue

---
+++ Blueprint-Blue-Lib/PEG/Parsed.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace BlueprintBlue.PEG
+{
+    [DataContract]
+    public class RootParse
+    {
+        [DataMember]
+        public string input { set; get; }
+        [DataMember]
+        public Parsed[] result { set; get; }
+        [DataMember]
+        public string error { set; get; }
+
+        public static (RootParse root, bool ok) Create(Stream stream)
+        {
+            try
+            {
+                var serializer = new DataContractJsonSerializer(typeof(RootParse));
+                var root = serializer.ReadObject(stream);
+                if (root != null)
+                    return ((RootParse) root, true);
+            }
+            catch
+            {
+                ;
+            }
+            return (new RootParse(), false);
+        }
+    }
+    [DataContract]
+    public class Parsed
+    {
+        [DataMember]
+        public string rule { set; get; }
+        [DataMember]
+        public string text { set; get; }
+        [DataMember]
+        public Parsed[] children { set; get; }
+    }
+    public class QuelleStatement
+    {
+        public string command { get; private set; }
+
+        public QuelleStatement(string stmt)
+        {
+            this.command = stmt;
+        }
+    }
+}
---
+++ BlueprintConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace BlueprintConsoleApp
+{
+    using BlueprintBlue;
+    using BlueprintBlue.PEG;
+
+    internal class Program
+    {
+        static void Print(IEnumerable<Parsed> parses, int level)
+        {
+            foreach(var parse in parses)
+            {
+                int paren = parse.rule.IndexOf('(');
+                string rule = paren > 0 ? parse.rule.Substring(0, paren).Trim() : parse.rule.Trim();
+                for (int i = 0; i < level; i++)
+                    Console.Write('\t');
+
+                Console.WriteLine(rule + ":\t" + parse.text.Trim());
+
+                if (parse.children.Length > 0)
+                {
+                    Program.Print(parse.children, level + 1);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            var blueprint = new Blueprint("http://127.0.0.1:3000/quelle");
+            var task = blueprint.Parse("\"\\foo\\ ... [he said] ... /pronoun/&/3p/\" + bar + x|y&z a&b&c > xfile < genesis 1:1");
+
+            if (task.IsCompleted)
+            {
+                var result = task.Result;
+
+                if (result != null)
+                {
+                    var error = result.error;
+                    if (!string.IsNullOrEmpty(error))
+                    {
+                        Console.WriteLine(error);
+                    }
+                    else
+                    {
+                        var list = result.result;
+                        Program.Print(list, 0);
+                    }
+                }
+            }
+        }
+    }
+}
