f454332f78c409188f7112ce218b4c37043d55f0
Kevin Wonus <kevin@wonus.com>
2023-03-05 22:27:58 -0500

    rust grpc client now builds

--- grpc/rust/blueprint-grpc/src/client.rs
+++ grpc/rust/blueprint-grpc/src/client.rs
@@ -1,20 +1,21 @@
+use std::io::stdin;
 use crate::blueprint::parse_statement_server::ParseStatement;
-use crate::blueprint::{ParseRequest, ParseResponse, QStatement, QExplicitCommand};
+use crate::blueprint::{ParseRequest, ParseResponse, QStatement, QExplicitCommand, parse_statement_client};
 
 mod blueprint;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = parse_statement_client::connect("http://[::1]:8080").await?;
+    let mut client = parse_statement_client::ParseStatementClient::connect("http://[::1]:8080").await?;
     loop {
-        println!("\nPlease vote for a particular url");/*
+        println!("\nPlease vote for a particular url");
         let mut u = String::new();
         println!("Please type a statement: ");
         stdin().read_line(&mut u).unwrap();
         let u = u.trim();
-        let blueprint::ParseRequest::new() = { StatementText: u };
+        let request = blueprint::ParseRequest { statement_text: String::from(u) };
         let response = client.parse(request).await?;
-        println!("Got: '{}' from service", response.into_inner().confirmation);*/
+        println!("Got: '{}' from service", response.into_inner().confirmation);
     }
     Ok(())
 }
