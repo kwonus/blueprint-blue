1b666a14a44a3ad2a3b7e89f9ef945e4b594a75d
Kevin Wonus <kevin@wonus.com>
2023-03-08 00:43:39 -0500

    Sync

--- grpc/rust/blueprint-grpc/src/server.rs
+++ grpc/rust/blueprint-grpc/src/server.rs
@@ -45,7 +45,6 @@ impl BlueprintBlue for BlueprintBlueService {
         &self,
         request: tonic::Request<ParseRequest>,
     ) -> Result<tonic::Response<ParseResponse>, tonic::Status> {
-
         let req = request.into_inner();
         let stmt = req.statement_text.clone();
 
@@ -55,16 +54,14 @@ impl BlueprintBlue for BlueprintBlueService {
             error: "".to_string(),
         };
 
-        let task = QuelleParser::parse(Rule::command, &stmt);
+        let task = QuelleParser::parse(Rule::statement, &stmt);
 
         if task.is_ok() {
             let pairs = task.unwrap();
             pinshot_recurse(pairs, &mut pinshot.result);
-        }
-        else if task.is_err() {
+        } else if task.is_err() {
             pinshot.error = task.unwrap_err().to_string();
-        }
-        else {
+        } else {
             pinshot.error = "internal error".to_string();
         }
 
@@ -72,23 +69,22 @@ impl BlueprintBlue for BlueprintBlueService {
             input: String::from("foo"),
             output: None,
             error_lines: vec![],
-            }),
+        }),
         );
         if !pinshot.error.is_empty() {
-            let mut lines = "some string 123 ffd".split("\n");
-            result.error_lines = lines.collect::<Vec<&str>>();
+            let lines = pinshot.error.split("\n");
+            /*for mut line in lines {
+                if Some(line) {
+                    result.unwrap().into_inner().error_lines.append(Ok(line).to_string());
+                }
+            }*/
+        } else {
+            result.unwrap().into_inner().output = pinshot_extract_statement(&pinshot.result);
         }
-        else {
-            result.output = pinshot_extract_statement(&pinshot.result);
-
-            match(result.output) {
-                Some(stmt) => result.output.u
-            }
-            Some(QStatement {
-                text: pinshot.result.,
-                is_valid: true,
-                is_explicit: pinshot.result.,
-                message: String::from(""),
+        result
+    }
+}
+/*
                 directives: Some(q_statement::Directives::Explicit(QExplicitCommand {
                     text: String::from("foo"),
                     verb: String::from("@Help"),
@@ -97,12 +93,7 @@ impl BlueprintBlue for BlueprintBlueService {
                         topic: String::from("bar"),
                     })),
                 })),
-            };
-        }
-        result
-    }
-
-}
+ */
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -116,14 +107,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 }
 
-fn pinshot_recurse(children: &Pairs<Rule>, items: &mut Vec<Parsed>)
+fn pinshot_recurse(children: Pairs<Rule>, items: &mut Vec<Parsed>)
 {
     for pair in children {
         let mut result: Vec<Parsed> = vec![];
         let text = pair.as_str().to_string();
         let rule = pair.to_string();
         // A non-terminal pair can be converted to an iterator of the tokens which make it up:
-        pinshot_recurse(&pair.into_inner(), &mut result);
+        pinshot_recurse(pair.into_inner(), &mut result);
 
         let item = Parsed {
             rule: rule,
@@ -141,7 +132,7 @@ fn pinshot_extract_statement(items: &Vec<Parsed>) -> Option<QStatement>
     for candidate in items {
         i = i + 1;
         if i == 1 {
-            is_explicit = candidate.children.len() == 1;
+            let mut is_explicit = candidate.children.len() == 1;
             if is_explicit {
                 for child in candidate.children {
                     is_explicit = candidate.rule.starts_with("@");
@@ -156,11 +147,14 @@ fn pinshot_extract_statement(items: &Vec<Parsed>) -> Option<QStatement>
                     message: "".to_string(),
                     directives: None,
                 });
-                if is_explicit {
-                    stmt.directives = Some(q_statement::Directives::Explicit(pinshot_extract_explcit_command(&candidate.children));
-                }
-                else if candidate.children.len() >= 1 {
-                    stmt.directives = Some(q_statement::Directives::Implicit(pinshot_extract_implicit_commands(&candidate.children));
+                if !candidate.children.is_empty() {
+                    let children = &candidate.children;
+                    if is_explicit {
+                        stmt.unwrap().directives = Some(q_statement::Directives::Explicit(pinshot_extract_explcit_command(&children).unwrap()));
+                    }
+                    else if candidate.children.len() >= 1 {
+                        stmt.unwrap().directives = Some(q_statement::Directives::Implicit(pinshot_extract_implicit_commands(&children).unwrap()));
+                    }
                 }
             }
         }
@@ -178,31 +172,5 @@ fn pinshot_extract_implicit_commands(items: &Vec<Parsed>) -> Option<QImplicitCom
 
 fn pinshot_extract_explcit_command(items: &Vec<Parsed>) -> Option<QExplicitCommand>
 {
-    let mut i: u8 = 0;
-    let mut stmt: Option<QStatement> = None;
-    for candidate in items {
-        i = i + 1;
-        if i == 1 {
-            is_explicit = candidate.children.len() == 1;
-            if is_explicit {
-                for child in candidate.children {
-                    is_explicit = candidate.rule.starts_with("@");
-                    break;
-                }
-            }
-            if candidate.rule == "statement" {
-                stmt = Some(QStatement {
-                    text: candidate.text.clone(),
-                    is_valid: true,
-                    is_explicit: is_explicit,
-                    message: "".to_string(),
-                    directives: pinshot_extract_commands(&candidate.children),
-                });
-            }
-        }
-        else {
-            return None;
-        }
-    }
-    stmt
+    None
 }
